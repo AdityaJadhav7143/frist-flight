516b0b42079702b7fbe9e104a2c69d545b99382d
AdityaJadhav7143 <Jadhavaj7143@gmail.com>
2023-04-28 21:00:19 +0530

    add all important topics

---
+++ src/app.js
@@ -0,0 +1,8 @@
+function App()
+{
+    return(
+        <h1>hello aditya</h1>
+
+    )
+}
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 
 import ReactDOM from 'react-dom/client';
+import App from "./app";
 
+const studentName = Math.random() ;
+
+function AwesomeButton()
+{
+    return(
+        <>
+        <button>click here!</button>
+        </>
+    )
+}
+
+function userCard()
+{
+    return(
+        <>
+        <h3>hii i am user card</h3>
+        <h4>i love java script</h4>
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h2>frist flight ✈️ </h2>);
+root.render(<>
+              <h1>frist flight {studentName} ✈️ </h1>
+              <h1>frist flight {70+73} ✈️ </h1>
+              {AwesomeButton()}
+
+              {userCard()} {userCard()}
+
+              {App()}
+
+           </>);
 
 
